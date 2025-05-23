630a042b98e1f111369fa12c9a9e4c1c7467347d
mauricioarrequena <mauricio.ar.requena@gmail.com>
2025-05-23 08:19:39 -0400

    first commit initial

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import './App.css'
+import Button from "@mui/material/button";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+
+export default function App() {
+  return (
+    <>
+      <Button variant='contained'>Hello world</Button>
+              <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DraftsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Drafts" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+    </>
+  )
+}
+
